25b9f6208366bba3449172c6e32d28b31f2abd86
Ewerton Madruga <emadruga@gmail.com>
2017-09-26 14:48:46 -0300

    doing emacs friendly indentation

--- src/providers/reddit/reddit.ts
+++ src/providers/reddit/reddit.ts
@@ -5,123 +5,123 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class RedditProvider {
 
-  settings: any;
-  loading: boolean = false;
-  posts: any = [];
-  subreddit: string = 'perfectloops';
-  page: number = 1;
-  perPage: number = 15;
-  after: string;
-  stopIndex: number;
-  sort: string = 'hot'
-  moreCount: number = 0;
+    settings: any;
+    loading: boolean = false;
+    posts: any = [];
+    subreddit: string = 'perfectloops';
+    page: number = 1;
+    perPage: number = 15;
+    after: string;
+    stopIndex: number;
+    sort: string = 'hot'
+    moreCount: number = 0;
 
-  constructor(public http: Http) {
+    constructor(public http: Http) {
 
-  }
-
-  fetchData(): void {
-
-    //Build the URL that will be used to access the API based on the users current preferences
-    let url = 'https://www.reddit.com/r/' + this.subreddit + '/' + this.sort + '/.json?limit='+ this.perPage;
-
-    //If we aren't on the first page, we need to add the after parameter so that we only get new results
-    //this parameter basically says "give me the posts that come AFTER this post"
-    if(this.after){
-      url += '&after=' + this.after;
     }
 
-    //We are now currently fetching data, so set the loading variable to true
-    this.loading = true;
+    fetchData(): void {
 
-    //Make a Http request to the URL and subscribe to the response
-    this.http.get(url).map(res => res.json()).subscribe(data => {
+	//Build the URL that will be used to access the API based on the users current preferences
+	let url = 'https://www.reddit.com/r/' + this.subreddit + '/' + this.sort + '/.json?limit='+ this.perPage;
 
-      let stopIndex = this.posts.length;
-      this.posts = this.posts.concat(data.data.children);
+	//If we aren't on the first page, we need to add the after parameter so that we only get new results
+	//this parameter basically says "give me the posts that come AFTER this post"
+	if(this.after){
+	    url += '&after=' + this.after;
+	}
 
-      //Loop through all NEW posts that have been added. We are looping through
-      //in reverse since we are removing some items.
-      for(let i = this.posts.length - 1; i >= stopIndex; i--){
+	//We are now currently fetching data, so set the loading variable to true
+	this.loading = true;
 
-        let post = this.posts[i];
+	//Make a Http request to the URL and subscribe to the response
+	this.http.get(url).map(res => res.json()).subscribe(data => {
 
-        //Add a new property that will later be used to toggle a loading animation
-        //for individual posts
-        post.showLoader = false;
-        post.alreadyLoaded = false;
+	    let stopIndex = this.posts.length;
+	    this.posts = this.posts.concat(data.data.children);
 
-        //Add a NSFW thumbnail to NSFW posts
-        if(post.data.thumbnail == 'nsfw'){
-          this.posts[i].data.thumbnail = 'images/nsfw.png';
-        }
+	    //Loop through all NEW posts that have been added. We are looping through
+	    //in reverse since we are removing some items.
+	    for(let i = this.posts.length - 1; i >= stopIndex; i--){
 
-          /*
-          * Remove all posts that are not in the .gifv or .webm format and convert the ones that
-          * are to .mp4 files.
-          */
-        if(post.data.url.indexOf('.gifv') > -1 || post.data.url.indexOf('.webm') > -1){
-          this.posts[i].data.url = post.data.url.replace('.gifv', '.mp4');
-          this.posts[i].data.url = post.data.url.replace('.webm', '.mp4');
+		let post = this.posts[i];
 
-              //If a preview image is available, assign it to the post as 'snapshot'
-          if(typeof(post.data.preview) != "undefined"){
-              this.posts[i].data.snapshot = post.data.preview.images[0].source.url.replace(/&amp;/g, '&');
-	      console.log(post);
-	      
-              //If the snapshot is undefined, change it to be blank so it doesnt use a broken image
-              if(this.posts[i].data.snapshot == "undefined"){
-		  this.posts[i].data.snapshot = "";
-              }
-          }
-            else {
-		this.posts[i].data.snapshot = "";
-            }
-        }
-        else {
-          this.posts.splice(i, 1);
-        }
-      }
+		//Add a new property that will later be used to toggle a loading animation
+		//for individual posts
+		post.showLoader = false;
+		post.alreadyLoaded = false;
 
-      //Keep fetching more GIFs if we didn't retrieve enough to fill a page
-      //But give up after 20 tries if we still don't have enough
-      if(data.data.children.length === 0 || this.moreCount > 20){
+		//Add a NSFW thumbnail to NSFW posts
+		if(post.data.thumbnail == 'nsfw'){
+		    this.posts[i].data.thumbnail = 'images/nsfw.png';
+		}
 
-        this.moreCount = 0;
-        this.loading = false;
+		/*
+		 * Remove all posts that are not in the .gifv or .webm format and convert the ones that
+		 * are to .mp4 files.
+		 */
+		if(post.data.url.indexOf('.gifv') > -1 || post.data.url.indexOf('.webm') > -1){
+		    this.posts[i].data.url = post.data.url.replace('.gifv', '.mp4');
+		    this.posts[i].data.url = post.data.url.replace('.webm', '.mp4');
 
-      }
-      else {
+		    //If a preview image is available, assign it to the post as 'snapshot'
+		    if(typeof(post.data.preview) != "undefined"){
+			this.posts[i].data.snapshot = post.data.preview.images[0].source.url.replace(/&amp;/g, '&');
+			console.log(post);
+			
+			//If the snapshot is undefined, change it to be blank so it doesnt use a broken image
+			if(this.posts[i].data.snapshot == "undefined"){
+			    this.posts[i].data.snapshot = "";
+			}
+		    }
+		    else {
+			this.posts[i].data.snapshot = "";
+		    }
+		}
+		else {
+		    this.posts.splice(i, 1);
+		}
+	    }
 
-        this.after = data.data.children[data.data.children.length - 1].data.name;
+	    //Keep fetching more GIFs if we didn't retrieve enough to fill a page
+	    //But give up after 20 tries if we still don't have enough
+	    if(data.data.children.length === 0 || this.moreCount > 20){
 
-        if(this.posts.length < this.perPage * this.page){
-          this.fetchData();
-          this.moreCount++;
-        } 
-        else {
-      this.loading = false;
-          this.moreCount = 0;
-        }
-      }
+		this.moreCount = 0;
+		this.loading = false;
 
-    }, (err) => {
-      //Fail silently, in this case the loading spinner will just continue to display
-      console.log("subreddit doesn't exist!");
-    });
-  }
+	    }
+	    else {
 
-  nextPage(){
-    this.page++;
-    this.fetchData();   
-  }
+		this.after = data.data.children[data.data.children.length - 1].data.name;
 
-  resetPosts(){
-      this.page = 1;
-      this.posts = [];
-      this.after = null;
-      this.fetchData();
-  }
+		if(this.posts.length < this.perPage * this.page){
+		    this.fetchData();
+		    this.moreCount++;
+		} 
+		else {
+		    this.loading = false;
+		    this.moreCount = 0;
+		}
+	    }
+
+	}, (err) => {
+	    //Fail silently, in this case the loading spinner will just continue to display
+	    console.log("subreddit doesn't exist!");
+	});
+    }
+
+    nextPage(){
+	this.page++;
+	this.fetchData();   
+    }
+
+    resetPosts(){
+	this.page = 1;
+	this.posts = [];
+	this.after = null;
+	this.fetchData();
+    }
 
 }
 
